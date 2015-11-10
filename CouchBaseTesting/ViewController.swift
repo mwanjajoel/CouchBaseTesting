fb452df0365928088c7fc45ca17a3c4d7a7961f9
Joel Mwanja <joel@a23labs.com>
2015-11-10 21:46:02 +0300

    Initial Commit

---
+++ CouchBaseTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CouchBaseTesting
+//
+//  Created by codebender on 11/10/15.
+//  Copyright © 2015 A23 Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
